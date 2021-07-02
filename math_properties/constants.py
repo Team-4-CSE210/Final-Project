9b68db14dd8010483164450b09721a106855892a
Mireya <mireyavera1511@gmail.com>
2021-07-02 16:26:38 -0600

    Added fruits images

--- math_properties/constants.py
+++ math_properties/constants.py
@@ -4,4 +4,4 @@ SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 700
 
 PATH = os.path.dirname(os.path.abspath(__file__))
-SHIP_SPRITE_PATH = PATH + "/assets/ship.jpg"
+SHIP_SPRITE_PATH = PATH + "/assets/paddle.png"
