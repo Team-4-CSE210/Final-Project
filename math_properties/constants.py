2ec7c51e740ea6e16a7a75a87f64efedb5f09440
Mireya <mireyavera1511@gmail.com>
2021-07-02 23:47:49 -0600

    falling_item fixed

--- math_properties/constants.py
+++ math_properties/constants.py
@@ -4,4 +4,13 @@ SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 700
 
 PATH = os.path.dirname(os.path.abspath(__file__))
-SHIP_SPRITE_PATH = PATH + "/assets/paddle.png"
+PADDLE_SPRITE_PATH = PATH + "/assets/paddle.png"
+BACKGROUND = PATH + "/assets/jungle.jpg"
+APPLE = PATH + "/assets/apple.png"
+BANANA = PATH + "/assets/banana.png"
+GRAPES = PATH + "/assets/grapes.png"
+KIWI = PATH + "/assets/kiwi.png"
+PINEAPPLE = PATH + "/assets/pineapple.png"
+STRAWBERRY = PATH + "/assets/strawberry.png"
+WATERMELON =  PATH + "/assets/watermelon.png"
+
